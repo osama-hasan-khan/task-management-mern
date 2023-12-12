e5db325aaae9cdb2425c8c169c2dd094e7b4d05c
osama-hasan-khan <osamahasankhan098@gmail.com>
2023-12-12 10:33:53 +0530

    create controllers, utils, models, routes, config for database connectivity and middleware folder for creating rest-api

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
 dotenv.config();
+connectDB();
+
+const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("SERVER IS RUNNING");
