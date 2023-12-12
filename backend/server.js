84a6792298167a5c442e99e85f08256fb51c2364
osama-hasan-khan <osamahasankhan098@gmail.com>
2023-12-12 11:13:57 +0530

    successfully created first user and test it with postman and for secure authorization I make use of jwt and token

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
+// packages
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
+// files
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,6 +19,8 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("SERVER IS RUNNING");
