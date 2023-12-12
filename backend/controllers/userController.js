37a910caa7a0d54c3d7c8c947b12c40f05476164
osama-hasan-khan <osamahasankhan098@gmail.com>
2023-12-12 14:01:38 +0530

    created login user login and sign-in login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,4 +32,31 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+      });
+
+      return;
+    }
+  }
+});
+
+export { createUser, loginUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/createUser", createUser);
+router.post("/loginUser", loginUser);
 
 export default router;
