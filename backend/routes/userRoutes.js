c3308ad2fd398e65c7afe8ec97f864b0a7ec45c8
osama-hasan-khan <osamahasankhan098@gmail.com>
2023-12-12 14:34:37 +0530

    created get all user rest logic with protect middleware route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,17 @@
 import express from "express";
 import {
   createUser,
+  getAllUsers,
   loginUser,
   logoutCurrentUser,
 } from "../controllers/userController.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/createUser", createUser);
 router.post("/loginUser", loginUser);
 router.post("/logoutUser", logoutCurrentUser);
+router.get("/getAllUsers", authenticate, getAllUsers);
 
 export default router;
