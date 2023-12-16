0f24b26d04f36a4d1297cdff12c5b054adb4d89d
osama-hasan-khan <osamahasankhan098@gmail.com>
2023-12-16 13:31:33 +0530

    Add Redux Toolkit for state management
    Integrate @reduxjs/toolkit for simplified Redux setup
    
    Create user slice to manage user-related state
    
    Implement actions and reducers for user authentication

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -4,11 +4,38 @@ import { Link } from "react-router-dom";
 import { CiUser } from "react-icons/ci";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SignUp = () => {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleSignUp = async () => {
+    try {
+      const res = await fetch("/api/users/createUser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName: userName,
+          email: email,
+          password: password,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        toast.error(data.error);
+        return;
+      }
+    } catch (error) {
+      toast.error(error);
+    }
+  };
 
   return (
     <div className="flex flex-row h-screen justify-between items-center mr-24">
@@ -22,7 +49,7 @@ const SignUp = () => {
         <h2 className="text-2xl font-myFont tracking-widest">
           Task Management Application
         </h2>
-        <form className="flex flex-col mt-8 gap-2">
+        <form className="flex flex-col mt-8 gap-2" onSubmit={handleSignUp}>
           <label htmlFor="" className="font-myFont tracking-widest">
             Username
           </label>
@@ -30,7 +57,11 @@ const SignUp = () => {
             <CiUser className="absolute left-3 top-2" />
             <input
               type="text"
+              name="userName"
+              required
               placeholder="John Doe"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
               className="outline-none border pl-8 border-slate-300 px-3 py-1 rounded-lg font-myFont tracking-widest w-full"
             />
           </div>
@@ -42,7 +73,11 @@ const SignUp = () => {
             <MdEmail className="absolute left-3 top-2" />
             <input
               type="text"
+              name="email"
+              required
               placeholder="Johndoe123@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="outline-none border pl-8 border-slate-300 px-3 py-1 rounded-lg font-myFont tracking-widest w-full"
             />
           </div>
@@ -53,14 +88,17 @@ const SignUp = () => {
             <RiLockPasswordFill className="absolute left-3 top-2" />
             <input
               type="password"
-              placeholder="**************"
+              name="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="outline-none border pl-8 border-slate-300 px-3 py-1 rounded-lg font-myFont tracking-widest w-full"
             />
           </div>
         </form>
 
         <Link
-          to="/dashboard"
+          onClick={handleSignUp}
           className="mt-3 px-3 py-1.5 bg-black text-white font-bold font-myFont tracking-widest text-center rounded-lg"
         >
           Sign up
