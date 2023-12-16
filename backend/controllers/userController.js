d4425892fbf33cc8e8addf82eda39d00ead8948d
osama-hasan-khan <osamahasankhan098@gmail.com>
2023-12-17 01:23:58 +0530

    rewrite login and signup rest api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,31 +4,36 @@ import bcrypt from "bcryptjs";
 import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
-  const { username, email, password } = req.body;
-
-  if (!username || !email || !password) {
-    throw new Error("Please fill all the inputs.");
-  }
-
-  const userExists = await User.findOne({ email });
-  if (userExists) res.status(400).send("User already exists");
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const newUser = new User({ username, email, password: hashedPassword });
-
   try {
-    await newUser.save();
-    createToken(res, newUser._id);
+    const { username, email, password } = req.body;
 
-    res.status(201).json({
-      _id: newUser._id,
-      username: newUser.username,
-      email: newUser.email,
-    });
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    await newUser.save();
+
+    if (newUser) {
+      createToken(res, newUser._id);
+
+      res.status(201).json({
+        _id: newUser._id,
+        email: newUser.email,
+        username: newUser.username,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
-    console.log("Error in creating user: ", err.message);
+    console.log("Error in signupUser: ", err.message);
   }
 });
 
@@ -36,27 +41,21 @@ const loginUser = asyncHandler(async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-    if (!existingUser) return res.status(400).json({ error: "Invalid email" });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-    if (existingUser) {
-      const isPasswordValid = await bcrypt.compare(
-        password,
-        existingUser.password
-      );
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ error: "Invalid email or password" });
 
-      if (isPasswordValid) {
-        createToken(res, existingUser._id);
+    if (isPasswordCorrect) {
+      createToken(res, user._id);
 
-        res.status(201).json({
-          _id: existingUser._id,
-          username: existingUser.username,
-          email: existingUser.email,
-        });
-
-        return;
-      }
+      res.status(200).json({
+        _id: user._id,
+        email: user.email,
+        username: user.username,
+      });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
